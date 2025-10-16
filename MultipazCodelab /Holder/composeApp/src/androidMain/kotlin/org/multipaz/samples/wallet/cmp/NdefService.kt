b18281626fc12c322c733b90a7b69a6c7bcf6d4c
hanluOMH <hanlu@openmobilehub.com>
2025-10-16 14:10:27 -0700

    Fix holder app can't be read by nfc, the fix tested by test app, it works. (#18)
    Code comes from testapp change
    
    rename

--- MultipazCodelab /Holder/composeApp/src/androidMain/kotlin/org/multipaz/samples/wallet/cmp/NdefService.kt
+++ MultipazCodelab /Holder/composeApp/src/androidMain/kotlin/org/multipaz/samples/wallet/cmp/NdefService.kt
@@ -1,26 +1,36 @@
 package org.multipaz.samples.wallet.cmp
 
 import org.multipaz.compose.mdoc.MdocNdefService
-import org.multipaz.crypto.EcCurve
+
 import org.multipaz.mdoc.transport.MdocTransportOptions
+import org.multipaz.util.Platform
 
 class NdefService: MdocNdefService() {
+    private lateinit var settingsModel: AppSettingsModel
 
     override suspend fun getSettings(): Settings {
+        val app = App.getInstance()
+        app.init()
+        
+        settingsModel = AppSettingsModel.create(
+            storage = Platform.storage,
+            readOnly = true
+        )
+
         return Settings(
-            sessionEncryptionCurve = EcCurve.P256,
-            allowMultipleRequests = false,
-            useNegotiatedHandover = true,
-            negotiatedHandoverPreferredOrder = listOf(
-                "ble:central_client_mode:",
-                "ble:peripheral_server_mode:",
+            sessionEncryptionCurve = settingsModel.presentmentSessionEncryptionCurve.value,
+            allowMultipleRequests = settingsModel.presentmentAllowMultipleRequests.value,
+            useNegotiatedHandover = settingsModel.presentmentUseNegotiatedHandover.value,
+            negotiatedHandoverPreferredOrder = settingsModel.presentmentNegotiatedHandoverPreferredOrder.value,
+            staticHandoverBleCentralClientModeEnabled = settingsModel.presentmentBleCentralClientModeEnabled.value,
+            staticHandoverBlePeripheralServerModeEnabled = settingsModel.presentmentBlePeripheralServerModeEnabled.value,
+            staticHandoverNfcDataTransferEnabled = settingsModel.presentmentNfcDataTransferEnabled.value,
+            transportOptions = MdocTransportOptions(
+                bleUseL2CAP = settingsModel.presentmentBleL2CapEnabled.value,
+                bleUseL2CAPInEngagement = settingsModel.presentmentBleL2CapInEngagementEnabled.value
             ),
-            staticHandoverBleCentralClientModeEnabled = false,
-            staticHandoverBlePeripheralServerModeEnabled = false,
-            staticHandoverNfcDataTransferEnabled = false,
-            transportOptions = MdocTransportOptions(bleUseL2CAP = true),
             promptModel = App.promptModel,
-            presentmentActivityClass = NfcActivity::class.java,
+            presentmentActivityClass = NfcActivity::class.java
         )
     }
 }
