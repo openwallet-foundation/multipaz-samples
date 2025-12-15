9781546fce9586c059efbbddf42ae182203d5b2f
Ever Morales <ever.morales@koombea.com>
2025-12-14 22:51:42 -0600

    Implement getAppToAppOrigin for iOS
    Signed-off-by: Ever Morales <ever.morales@koombea.com>

--- MultipazGettingStartedSample/composeApp/src/iosMain/kotlin/org/multipaz/getstarted/GetAppOrigin.kt
+++ MultipazGettingStartedSample/composeApp/src/iosMain/kotlin/org/multipaz/getstarted/GetAppOrigin.kt
@@ -1,5 +1,10 @@
 package org.multipaz.getstarted
 
+import platform.Foundation.NSBundle
+
 actual fun getAppToAppOrigin(): String {
-    TODO("Add support for iOS")
+    // On iOS, use the bundle identifier as the app origin
+    // This uniquely identifies the app and is the iOS equivalent
+    // of using the signing certificate on Android
+    return NSBundle.mainBundle.bundleIdentifier ?: "unknown.bundle.id"
 }
