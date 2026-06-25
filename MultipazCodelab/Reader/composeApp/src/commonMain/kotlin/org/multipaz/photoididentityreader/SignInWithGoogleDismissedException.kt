57cdaf3818649c475afeb2134c9c3a732715c0bf
VishnuSanal <vishnusanalt@gmail.com>
2026-06-25 15:42:07 +0530

    remove outdated codelab reader app
    Signed-off-by: VishnuSanal <vishnusanalt@gmail.com>

--- MultipazCodelab/Reader/composeApp/src/commonMain/kotlin/org/multipaz/photoididentityreader/SignInWithGoogleDismissedException.kt
+++
@@ -1,6 +0,0 @@
-package org.multipaz.photoididentityreader
-
-/**
- * Thrown by [signInWithGoogle] if the user dismisses the UI prompt.
- */
-class SignInWithGoogleDismissedException(message: String, cause: Throwable?): Exception(message, cause)
