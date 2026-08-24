b209696e474b59d407d51e42fb4412e7eb103ddc
Hanlu Feng <hanlu@openmobilehub.com>
2026-08-24 15:24:48 -0700

    Fix Holder iOS build setup
    Fixes/Addresses #131
    
    Move Holder iOS signing identifiers into a local DeveloperConfig.xcconfig file so developers do not need to manually edit team IDs, bundle IDs, or App Group IDs in Xcode. The app, extension entitlements, and runtime storage lookup now all read from the same config-expanded AppGroupID value.
    
    Also wire the Xcode-triggered Kotlin framework task to prepare Compose resources automatically before embedding the iOS framework.
    
    fix #131
    
    This removes repeated manual iOS setup in Xcode and prevents local developer signing values from being hardcoded in the project. It also removes the manual Compose resource preparation step while avoiding a clean/resource-generation race during Xcode builds.
    
    Signed-off-by: Hanlu Feng <hanlu@openmobilehub.com>

--- MultipazCodelab/Holder/composeApp/src/iosMain/kotlin/org/multipaz/samples/wallet/cmp/util/PlatformStorage.ios.kt
+++ MultipazCodelab/Holder/composeApp/src/iosMain/kotlin/org/multipaz/samples/wallet/cmp/util/PlatformStorage.ios.kt
@@ -2,16 +2,17 @@ package org.multipaz.samples.wallet.cmp.util
 
 import org.multipaz.storage.Storage
 import org.multipaz.storage.ios.IosStorage
+import platform.Foundation.NSBundle
 import platform.Foundation.NSFileManager
-
-private const val IOS_APP_GROUP_IDENTIFIER = "group.org.multipaz.hanlu.testapp.sharedgroup"
 
 actual fun createWalletStorage(): Storage =
     IosStorage(
         storageFileUrl =
             NSFileManager.defaultManager
                 .containerURLForSecurityApplicationGroupIdentifier(
-                    groupIdentifier = IOS_APP_GROUP_IDENTIFIER,
+                    groupIdentifier =
+                        NSBundle.mainBundle.objectForInfoDictionaryKey("AppGroupID") as? String
+                            ?: error("Missing AppGroupID in Info.plist"),
                 )!!
                 .URLByAppendingPathComponent("storageNoBackup.db")!!,
         excludeFromBackup = true,
